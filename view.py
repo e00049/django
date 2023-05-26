31b11cb9d99662de17f56d90fe7463af9dcd8a82
e00049 <105837578+e00049@users.noreply.github.com>
2023-05-26 18:31:55 +0530

    Update view.py

--- view.py
+++ view.py
@@ -6,8 +6,8 @@ def fetch_values(request):
 
     single_object    = YourTable.objects.get(id=some_id)     # Retrieve a single object
 
-		profile          = YourTable.object.create(user = user, phone_number = phone_number)  - Create an object
+    profile          = YourTable.object.create(user = user, phone_number = phone_number)  - Create an object
 
-		delete_user      = YourTable.objects.get(id=1).delete()
+    delete_user      = YourTable.objects.get(id=1).delete()
 
     Profile.objects.filter(phone_number=phone_number).update(otp=str(random.randint(100000, 999999)))
