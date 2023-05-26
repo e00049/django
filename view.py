46407ad668d3a02d822e5fdb86265bfaeed1559a
e00049 <105837578+e00049@users.noreply.github.com>
2023-05-26 18:31:33 +0530

    Create view.py

---
+++ view.py
@@ -0,0 +1,13 @@
+def fetch_values(request):
+
+    all_objects      = YourTable.objects.all() 	    # Retrieve all objects from the table
+
+    filtered_objects = YourTable.objects.filter(phone_number = 7358220899)     # Retrieve objects based on a condition
+
+    single_object    = YourTable.objects.get(id=some_id)     # Retrieve a single object
+
+		profile          = YourTable.object.create(user = user, phone_number = phone_number)  - Create an object
+
+		delete_user      = YourTable.objects.get(id=1).delete()
+
+    Profile.objects.filter(phone_number=phone_number).update(otp=str(random.randint(100000, 999999)))
