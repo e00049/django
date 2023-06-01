a0abb67a8616849493217c5a7e17e7a6532c894d
e00049 <105837578+e00049@users.noreply.github.com>
2023-06-01 21:23:30 +0530

    Create admin.py

---
+++ admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from apptrainersportal.models import Product
+
+# Register your models here.
+
+class apptrainersportal(admin.ModelAdmin):
+    list = [ 'name', 'price' ]
+
+admin.site.register(Product, apptrainersportal)
